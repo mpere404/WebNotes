e31238163a7763f4854aa4b791002a617aa02123
Michael Perez <mpere692@fiu.edu>
2021-09-25 18:20:02 -0400

    Initial WebNotes ASP.NET Web Forms MVC Project Commit

---
+++ WebNotes/WebNotes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebNotes.Startup))]
+namespace WebNotes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
